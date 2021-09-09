933ebd4477a315046436b9303601659f5434ac91
WILLIAM DE SA RODRIGUES <wsr2k2@gmail.com>
2021-09-09 08:57:01 -0300

    correção

--- Aulas_Blue/Aula18/exer_funcoes.js
+++ Aulas_Blue/Aula18/exer_funcoes.js
@@ -1,4 +1,4 @@
-const prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")();
 /*Exercício para praticar funções
 Nome: Joguinho: Salão
 Objetivo: Escrever uma função que funciona como um jogo, em que a pessoa digitar as suas escolhas e o programa irá indicar o resultado das escolhas.
@@ -16,21 +16,39 @@ Crie uma função principal chamada jogo() e execute as tarefas abaixo:
 */
 
 function jogo() {
-    console.log("\033[1;33m========================================================================");
-    console.log("                    BEM VINDO AO JOGO - O SALÃO");
-    console.log("========================================================================");
-    console.log("\033[1;37mVocê acaba de entrar em um salão desconhecido, onde tudo pode acontecer!");
-        console.log("À sua frente existem duas portas, escolha uma delas e prepare-se para o desconhecido. Qual porta você irá escolher?");
-        let escolha = prompt("\033[1;34m Direita ou Esquerda? ");
-        if (escolha.toUpperCase() == "Esquerda".toUpperCase() || escolha.toUpperCase() == "e".toUpperCase() ){
-        return console.log(`Você entrou na sala à ESQUERDA, parece que não tem nada por aqui. Acho melhor você voltar correndo!`);
-        } else if ((escolha.toUpperCase() == "Direita".toUpperCase() || escolha.toUpperCase() == "d".toUpperCase() )){
-        return console.log("Excelente escolha! A sala à DIREITA estava te esperando esse tempo todo! Que maravilhoso isso. Pena que não tem nada aqui para você.");
-        } else {
-            return console.log("Você não escolheu nenhuma das portas. Tente novamente.");
-        }
-
-
+  console.log(
+    "\033[1;33m========================================================================"
+  );
+  console.log("                    BEM VINDO AO JOGO - O SALÃO");
+  console.log(
+    "========================================================================"
+  );
+  console.log(
+    "\033[1;37mVocê acaba de entrar em um salão desconhecido, onde tudo pode acontecer!"
+  );
+  console.log(
+    "À sua frente existem duas portas, escolha uma delas e prepare-se para o desconhecido. Qual porta você irá escolher?"
+  );
+  let escolha = prompt("\033[1;34m Direita ou Esquerda? ");
+  if (
+    escolha.toUpperCase() == "Esquerda".toUpperCase() ||
+    escolha.toUpperCase() == "e".toUpperCase()
+  ) {
+    return console.log(
+      `Você entrou na sala à ESQUERDA, parece que não tem nada por aqui. Acho melhor você voltar correndo!`
+    );
+  } else if (
+    escolha.toUpperCase() == "Direita".toUpperCase() ||
+    escolha.toUpperCase() == "d".toUpperCase()
+  ) {
+    return console.log(
+      "Excelente escolha! A sala à DIREITA estava te esperando esse tempo todo! Que maravilhoso isso. Pena que não tem nada aqui para você."
+    );
+  } else {
+    return console.log(
+      "Você não escolheu nenhuma das portas. Tente novamente."
+    );
+  }
 }
 
 jogo();
